538e28d760350cc6a0cbe375775a76bb9e9b608e
Colin Shea <colin@evaryont.me>
2010-08-28 20:47:09 -0400

    We use YAJL and Digest now.

--- lib/indelible.rb
+++ lib/indelible.rb
@@ -1,9 +1,7 @@
 $: << "lib/indelible"
 require 'rubygems'
-# require 'socket'
-# require 'thread'
-require 'json'
-require 'md5'
+require 'yajl/json_gem'
+require 'digest/md5'
 require 'daemons'
 require 'simplenote'
 
